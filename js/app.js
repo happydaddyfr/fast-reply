e5d2caa467ebb69519d8eb7b05dfea5feda810d3
Unknown <olivier.roger@gmail.com>
2018-05-10 09:33:56 +0200

    Changements des filtres
    - pas de truncate en milieu de mot
    - support de la transformation du markdown en HTML

--- js/app.js
+++ js/app.js
@@ -65,7 +65,22 @@ var vm = new Vue({
 	  truncate: function (value, length) {
 	    if (!value) return '';
 	    if (value.length <= length) return value;
-	    return value.substring(0, length) + " [...]";
+
+	    function truncateOnWord(str, limit) {
+	        var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
+	        var reg = new RegExp('(?=[' + trimmable + '])');
+	        var words = str.split(reg);
+	        var count = 0;
+	        return words.filter(function(word) {
+	            count += word.length;
+	            return count <= limit;
+	        }).join('');
+	    }
+
+	    return truncateOnWord(value, length) + " [...]";
+	  },
+	  markdownToHTML: function(value) {
+	  	return new showdown.Converter().makeHtml(value);
 	  }
 	},
 	methods: {
