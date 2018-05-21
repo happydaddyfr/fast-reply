4acc843d25a61ea0493b1b5b024da143f6ed3688
Unknown <olivier.roger@gmail.com>
2018-05-21 14:47:47 +0200

    Add close button to toast notifications

--- src/utils/toast.js
+++ src/utils/toast.js
@@ -7,7 +7,13 @@ export default {
   createDialog: function (type, data, timeout) {
     Vue.toasted[type](data, {
       position: 'bottom-right',
-      duration: timeout
+      duration: timeout,
+      action: {
+        text: 'Close',
+        onClick: (e, toastObject) => {
+          toastObject.goAway(0)
+        }
+      }
     })
   }
 }
